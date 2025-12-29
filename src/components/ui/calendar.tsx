da2380d011b7d830cd842a0e50f04ba67fc18046
Santanu Das <gamingbeast77yt@gmail.com>
2025-12-29 18:42:21 +0000

    remove (_for element <Weekday>_)

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -24,7 +24,7 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-sm font-medium text-center",
         nav: "hidden",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -33,7 +33,7 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+        head_row: "hidden",
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
