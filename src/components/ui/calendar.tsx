82a0d2c118767de5f0b4a4c331b2e5860c40e92e
Santanu Das <gamingbeast77yt@gmail.com>
2025-12-29 18:07:03 +0000

    Object literal may only specify known properties, and 'IconLeft' does no

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -51,10 +51,8 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+      IconLeft={() => <ChevronLeft className="h-4 w-4" />}
+      IconRight={() => <ChevronRight className="h-4 w-4" />}
       {...props}
     />
   )
